8adad60562a5ca2b4774c46a169207f6096a70ef
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2025-03-12 09:53:33 +0100

    Ansible 12/03/25

--- fuentes/Ansible/Instalar JupyterHub con docker (falta comprovar)/cargar_jupyterhub.sh
+++ fuentes/Ansible/Instalar JupyterHub con docker (falta comprovar)/cargar_jupyterhub.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Copiar el script remove_comments.sh al contenedor de JupyterHub
+# region Copiar el script remove_comments.sh al contenedor de JupyterHub
 docker cp remove_comments.sh jupyterhub:/srv/jupyterhub/Eliminar_comentarios.sh
+# endregion
 
-# Acceder al contenedor de JupyterHub para realizar la instalación y la configuración
+# region Acceder al contenedor de JupyterHub para realizar la instalación y la configuración
 docker exec -it jupyterhub bash -c "
     # Instalar dependencias necesarias (si no están instaladas)
     command -v jupyterhub >/dev/null 2>&1 || {
@@ -22,3 +23,10 @@ docker exec -it jupyterhub bash -c "
     # Llamar al script para eliminar los comentarios en el archivo de configuración
     bash /srv/jupyterhub/Eliminar_comentarios.sh
 "
+# endregion
+
+# region Reiniciar el contenedor de JupyterHub
+exit
+docker restart jupyterhub
+# endregion
+
